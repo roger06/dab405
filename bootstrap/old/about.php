6017847be04b797a452f1655a1e562055848b02d
Roger Holden <r.holden@chi.ac.uk>
2025-03-06 11:39:32 +0000

    week 4

---
+++ bootstrap/old/about.php
@@ -0,0 +1,28 @@
+<?php
+
+    include("header-inc.php");
+?>
+
+    <main role="main" class="container">
+
+      <div class="starter-template">
+        <h1>About pizza evening!</h1>
+        <p class="lead">Where is the pizza!????</p>
+      </div>
+        
+        <div class="container">
+  <div class="row">
+    <div class="col-sm" style="background-color: blueviolet">
+      One of three columns
+    </div>
+    <div class="col-sm" style="background-color: red">
+      One of three columns
+    </div>
+    <div class="col-sm" style="background-color: green">
+      One of three columns
+    </div>
+  </div>
+</div>
+        
+        
+   <?php include("footer-inc.php");?>
---
+++ bootstrap/old/bstest.php
@@ -0,0 +1,28 @@
+<?php
+
+    include("header-inc.php");
+?>
+
+    <main role="main" class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+        
+        <div class="container">
+  <div class="row">
+    <div class="col-sm" style="background-color: blueviolet">
+      One of three columns
+    </div>
+    <div class="col-sm" style="background-color: red">
+      One of three columns
+    </div>
+    <div class="col-sm" style="background-color: green">
+      One of three columns
+    </div>
+  </div>
+</div>
+        
+        
+   <?php include("footer-inc.php");?>
